7d489dcfc3e9b01941a7d3d71fd223ad92eb0f87
Piotr Milewski <milewski.piotr@gmail.com>
2023-10-19 19:07:56 +0000

    Random Quote

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
+import {gql, useQuery} from "@apollo/client" 
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Inspiring Quote</h1>
-      <Quote text="to be" author="ha" />
+      <RandomQuote />
     </div>
   );
+}
+
+const RANDOM_QUOTE_QUERY = gql`
+  query RandomQuoteGet{
+    randomQuote {
+      text
+      author
+    }
+  }
+`
+
+function RandomQuote() {
+  const { data, loading, error, refetch } = useQuery(RANDOM_QUOTE_QUERY, {
+    fetchPolicy: "no-cache"
+  })
+  if (loading) {
+    return "Quote is loading"
+  }
+
+  if (error) {
+    return `Error: ${error.message}`;
+  }
+  const { text, author } = data.randomQuote;
+
+  return (
+    <>
+      <Quote text={text} author={author} />
+      <button
+        onClick={() => { 
+          refetch();
+        }}
+      >Get new quote</button>
+    </>
+  )
 }
 
 function Quote({ text, author }) {
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 import App from "./App";
+
+// const URL = "https://x5sfpd-4000.csb.app/";
+// const URL = "http://localhost:4000"
+const URL = "https://zgcrm.sse.codesandbox.io/"
+
+const client = new ApolloClient({
+  uri: URL,
+  cache: new InMemoryCache(),
+  queryDeduplication: false
+});
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <ApolloProvider client={client} >
+      <App />
+    </ApolloProvider>
+  </StrictMode>,
 );
